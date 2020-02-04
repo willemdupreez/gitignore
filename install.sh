8b3e72409d358c9319adefbaf0aa8a4679d1f025
Willem du Preez <will@runoptimised.net>
2020-02-04 10:23:53 +0000

    Added install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+echo '# === Visual Studio Code ===\n' > ~/.gitignore
+cat ./Global/VisualStudioCode.gitignore >> ~/.gitignore
+echo '\n# === macOS ===\n' >> ~/.gitignore
+cat ./Global/macOS.gitignore >> ~/.gitignore

6388948ee8243ba001dbf6077d223724a885767f
Willem du Preez <will@runoptimised.net>
2020-02-04 10:29:36 +0000

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
+echo -e 'Creating new global .gitignore...'
+echo -e '  Adding VS Code...'
 echo '# === Visual Studio Code ===\n' > ~/.gitignore
 cat ./Global/VisualStudioCode.gitignore >> ~/.gitignore
+
+echo -e '  Adding macOS...'
 echo '\n# === macOS ===\n' >> ~/.gitignore
 cat ./Global/macOS.gitignore >> ~/.gitignore
+
+echo -e 'Setting file as global excludes file...'
+git config --global core.excludesfile ~/.gitignore
+
+echo -e 'Done.'
